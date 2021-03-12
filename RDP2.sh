eca97fbca3b31a6f3c7a3904999e1872e356a6a2
AKYTRC <80533057+AKYTRC@users.noreply.github.com>
2021-03-12 20:52:00 +0530

    Create RDP2.sh

---
+++ RDP2.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+printf "RDP installing... " >&2
+{
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install -y obs-studio
+sudo apt install -y firefox
+sudo apt install -y falkon
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+apt update
+apt install anydesk -y
+sudo adduser ALOK chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Completed " >&2 ||
+printf "\nError Occured " >&2
+printf '\nGO to https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - ALOK -c """$CRP"""
+printf 'Your SUDO Password Is 1234 \n'
+printf 'YT Channel - https://www.youtube.com/c/AKYTRC \n'
