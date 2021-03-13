d89d2bf987b2fe72a63ffe2b7b413da79e5bea86
AKYTRC <80533057+AKYTRC@users.noreply.github.com>
2021-03-13 13:40:23 +0530

    Update RDP3.sh

--- RDP3.sh
+++ RDP3.sh
@@ -4,9 +4,9 @@ printf "RDP installing...\n" >&2
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
-sudo apt -y install screen
-sudo apt -y install firefox
-sudo apt install tightvncserver
+sudo apt install screen -y
+sudo apt install firefox -y
+sudo apt install tightvncserver -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 } &> /dev/null &&
@@ -15,10 +15,6 @@ printf "\nError Occured\n " >&2
 tightvncserver :1
 clear
 printf '\nEnter the Authentication code\n'
-
-printf "screen ./ngrok tcp 5901"\n" >&2
-{
 read -p " "
 screen ./ngrok tcp 5901"
-} &> /dev/null &&
-printf "\nRunning...\n " >&2 
+
